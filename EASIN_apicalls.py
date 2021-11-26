cc43599bf47151d6610a90a44be32d01d4a4b08a
widadmogral <widadmogral@gmail.com>
2021-11-26 16:03:21 +0100

    removing unnecessary comments

--- EASIN_apicalls.py
+++ EASIN_apicalls.py
@@ -2,8 +2,7 @@ import requests
 def api_call_by_kingdom(kingdom, f):
     pagination_max = 50 #EASIN Api calls return only 50 records at a time
     skip_records = 0
-    url = 'https://easin.jrc.ec.europa.eu/api/cat/kingdom/'+kingdom+'/take/' + str(pagination_max) + '/skip/' + str(
-        skip_records)
+    url = 'https://easin.jrc.ec.europa.eu/api/cat/kingdom/'+kingdom+'/take/' + str(pagination_max) + '/skip/' + str(skip_records)
     r = requests.get(url)
     header_length = 1000
     tot_length = 0
@@ -13,8 +12,7 @@ def api_call_by_kingdom(kingdom, f):
             print(content['results'][i]['SpeciesName'], file=f)
             print(content['results'][i]['SpeciesName'])
         skip_records += 50
-        url = 'https://easin.jrc.ec.europa.eu/api/cat/kingdom/animalia/take/' + str(pagination_max) + '/skip/' + str(
-            skip_records)
+        url = 'https://easin.jrc.ec.europa.eu/api/cat/kingdom/animalia/take/' + str(pagination_max) + '/skip/' + str(skip_records)
         r = requests.get(url)
         header_length = int(r.headers['Content-Length'])
         tot_length += len(content['results'])
@@ -31,8 +29,3 @@ if __name__ == "__main__":
     main()
 
 
-'''
-
-print(r.json())
-print(r.status_code)
-'''
