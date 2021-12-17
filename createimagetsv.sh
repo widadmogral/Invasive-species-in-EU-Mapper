d603816061f6dbb66ec07ac18b13818c9f03d396
widadmogral <widadmogral@gmail.com>
2021-12-17 14:56:12 +0100

    scripts to create final tsv file with only required columns

--- createimagetsv.sh
+++ createimagetsv.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+head -n1 $1 > $2
 awk -v FS="\t" -v OFS="\t" '{print $1,$3,$4,$14}' $1  > $2
--- createtsv.sh
+++ createtsv.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-awk -v FS="\t" -v OFS="\t" '{print $1,$62,$99,$121,$133,$134,$233}' $1  > $2
+# add header to tsv file
+head -n1 $1 > $2 
+#add rest of rows
+awk -v FS="\t" -v OFS="\t" '{print $1,$62,$99,$121,$133,$134,$220,$233}' $1  > $2
