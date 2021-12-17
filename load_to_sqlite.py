766bac12e978d2e1e55083beb7be08741c04ea3a
widadmogral <widadmogral@gmail.com>
2021-12-17 19:47:50 +0100

    add insert or ignore sql statement to avoid primary key conflicts

--- load_to_sqlite.py
+++ load_to_sqlite.py
@@ -1,6 +1,19 @@
 import sqlite3
 import csv
+def load_to_db(csvfile):
+    con = sqlite3.connect('euinvasive.sqlite')
+    cur = con.cursor()
+    with open(csvfile, 'rt', encoding='utf-8') as f:  # default is that first line are the headers
+        dr = csv.DictReader(f, delimiter='\t')  # comma is default delimiter,changing it to \t
+        next(dr)  # skip header
+        to_db = [(i['gbifID'], i['datasetName'], i['eventDate'], i['countryCode'], i['decimalLatitude'],
+                  i['decimalLongitude'], i['taxonKey'], i['verbatimScientificName']) for i in dr]
 
+    cur.executemany(
+        "INSERT OR IGNORE INTO occurrences (gbifid, datasetName, eventTime, countryCode, decimalLatitude, decimalLongitude, taxonkey, verbatimScientificName) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+        to_db)
+    con.commit()
+    con.close()
 qry = open('create_occurrences.sql', 'r').read()
 conn = sqlite3.connect('euinvasive.sqlite')
 cur = conn.cursor()
@@ -8,28 +21,4 @@ cur.executescript(qry)
 cur.close()
 conn.commit()
 conn.close()
-'''
-with open('loadtodb.tsv', 'r') as f:
-    next(f) # Skip the header row.
-    cur.copy_from(f, 'occurrences', sep='\t', null = "")
-
-conn.commit()
-'''
-
-def load_to_db(csvfile):
-    con = sqlite3.connect('euinvasive.sqlite')
-    cur = con.cursor()
-    with open(csvfile, 'rt', encoding='utf-8') as f:  # default is that first line are the headers
-        dr = csv.DictReader(f, delimiter='\t')  # comma is default delimiter,changing it to \001
-        next(dr)  # skip header
-        to_db = [(i['gbifID'], i['datasetName'], i['eventDate'], i['countryCode'], i['decimalLatitude'],
-                  i['decimalLongitude'], i['taxonKey'], i['verbatimScientificName']) for i in dr]
-
-    cur.executemany(
-        "INSERT INTO occurrences (gbifid, datasetName, eventTime, countryCode, decimalLatitude, decimalLongitude, taxonkey, verbatimScientificName) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-        to_db)
-    con.commit()
-    con.close()
-
-
 load_to_db('loadtodb.tsv')
