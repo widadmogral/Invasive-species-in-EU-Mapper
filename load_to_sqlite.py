b7366e54e321d887c0bb9e66bfacb61b0a151525
widadmogral <widadmogral@gmail.com>
2021-12-21 19:22:26 +0100

    add wikipedia extraction

--- load_to_sqlite.py
+++ load_to_sqlite.py
@@ -1,13 +1,10 @@
 import csv
 import sqlite3
-
-
 def load_to_occurrences(csvfile):
     con = sqlite3.connect('euinvasive.sqlite')
     cur = con.cursor()
     with open(csvfile, 'rt', encoding='utf-8') as f:  # default is that first line are the headers
         dr = csv.DictReader(f, delimiter='\t')  # comma is default delimiter,changing it to \t
-        next(dr)  # skip header
         to_db = [(i['gbifID'], i['datasetName'], i['eventDate'], i['countryCode'], i['decimalLatitude'],
                   i['decimalLongitude'], i['taxonKey'], i['verbatimScientificName']) for i in dr]
 
@@ -30,13 +27,26 @@ def load_to_occurrence_images(csvfile):
     cur = con.cursor()
     with open(csvfile, 'rt', encoding='utf-8') as f:  # default is that first line are the headers
         dr = csv.DictReader(f, delimiter='\t')  # comma is default delimiter,changing it to \t
-        next(dr)  # skip header
         to_db = [(i['gbifID'], i['identifier'], i['license'], i['rightsHolder']) for i in dr]
 
     cur.executemany(
         "INSERT OR IGNORE INTO occurrence_images (gbifid, image_url, license, rights_holder) VALUES (?, ?, ?, ?);",to_db)
     con.commit()
     con.close()
+
+def load_to_wiki_info(csvfile):
+    con = sqlite3.connect('euinvasive.sqlite')
+    cur = con.cursor()
+    with open(csvfile, 'rt', encoding='utf-8') as f:  # default is that first line are the headers
+        dr = csv.DictReader(f, delimiter='\001')  # comma is default delimiter,changing it to \001
+        to_db = [(i['verbatimScientificName'], i['wiki_url'], i['summary']) for i in dr]
+
+    cur.executemany(
+        "INSERT OR IGNORE INTO wiki_info (scientific_name,wiki_url, summary) VALUES (?, ?, ?);",
+        to_db)
+    con.commit()
+    con.close()
+
 
 
 def create_db():
@@ -52,3 +62,4 @@ def create_db():
 create_db()
 load_to_occurrences('loadtodb.tsv')
 load_to_occurrence_images('media.tsv')
+load_to_wiki_info('wiki_info.csv')
---
+++ wikipedia_extract.py
@@ -0,0 +1,28 @@
+import wikipedia
+import csv
+import sys
+def main(specieslist):
+    url ="https://en.wikipedia.org/wiki/"
+    csvfile = open('wiki_info.csv', 'w')
+    f = open(specieslist,'r')
+    writer = csv.writer(csvfile, delimiter='\001')
+    writer.writerow(["verbatimScientificName","wiki_url","summary"])
+    names = [line.rstrip() for line in f]
+    titles = [wikipedia.search(name,results =1) for name in names]
+    urls =[url + title[0] for title in  titles ]
+    pages = [wikipedia.page(title = title[0]) for title in titles]
+    contents = [page.content for page in pages]
+    for i in range(0,len(names)):
+        csvname = r'{}'.format(names[i]) # using raw format to avoid interpretation of content
+        csvurl = r'{}'.format(urls[i])
+        csvcontent = r'{}'.format(contents[i])
+        writer.writerow([csvname,csvurl,csvcontent])
+    csvfile.close()
+    f.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        specieslist = sys.argv[1]
+    else:
+        specieslist = 'invasive_species_eu.txt'
+    main(specieslist)
