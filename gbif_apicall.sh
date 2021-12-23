4616929d67fc758e18750d78f00d44863aec85e2
widadmogral <widadmogral@gmail.com>
2021-12-23 19:33:23 +0100

    adding tmp directory

--- gbif_apicall.sh
+++ gbif_apicall.sh
@@ -1,4 +1,4 @@
-jq '.creator=env.GBIF_USER' query.json >> tmp.json && mv tmp.json query.json
-jq '.notificationAddresses[0]=env.GBIF_EMAIL' query.json >> tmp.json && mv tmp.json query.json
-curl --include --user $GBIF_USER:$GBIF_PWD --header Content-Type:application/json --data @query.json https://api.gbif.org/v1/occurrence/download/request
+tmp/jq '.creator=env.GBIF_USER' query.json >> tmp.json && mv tmp.json query.json
+tmp/jq '.notificationAddresses[0]=env.GBIF_EMAIL' query.json >> tmp.json && mv tmp.json query.json
+curl --include --user $GBIF_USER:$GBIF_PWD --header Content-Type:application/json --data @query.json https://api.gbif.org/v1/occurrence/download/request > tmp/gbifcurlresponse.txt
 
